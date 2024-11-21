d76cda023e030fcf8951161de85e9160f4a8d43a
unknown <nagallo.johnleonardo@gmail.com>
2024-11-21 15:52:02 +0800

    charts

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router();
 const { newOrder, getMonthlySales } = require('../controllers/order')
+const { isAuthenticatedUser } = require('../middlewares/auth');
 
+router.post('/order/new', isAuthenticatedUser, newOrder);
 router.get('/admin/monthly-sales', getMonthlySales);
 module.exports = router;
