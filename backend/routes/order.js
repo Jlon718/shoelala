3dc367ab510b68d5ff2858727aa5e74ee95b15d8
joe <139723099+jojotrance@users.noreply.github.com>
2024-11-21 14:04:32 +0800

    Transactions 1/2
    malapit na matapos

--- backend/routes/order.js
+++ backend/routes/order.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router();
 const { newOrder, salesPerMonth } = require('../controllers/order')
+const { isAuthenticatedUser } = require('../middlewares/auth');
+
+
+router.post('/order/new', isAuthenticatedUser, newOrder);
 
 router.get('/admin/sales-per-month', salesPerMonth);
 module.exports = router;
-
-
