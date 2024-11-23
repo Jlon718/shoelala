1f795f3f755f10642dbff5151f5f3495d639ce85
John Leonard O. Nagallo <139086373+Jlon718@users.noreply.github.com>
2024-11-23 16:00:33 +0800

    updated na talaga

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,46 +1,6 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
-<<<<<<< Updated upstream
-// exports.isAuthenticatedUser = async (req, res, next) => {
-    
-//     const token  = req.header('Authorization').split(' ')[1];
-
-
-//     if (!token) {
-//         return res.status(401).json({message:'Login first to access this resource'})
-//     }
-
-//     const decoded = jwt.verify(token, process.env.JWT_SECRET)
-//     req.user = await User.findById(decoded.id);
-    
-//     next()
-// };
-
-exports.isAuthenticatedUser = async (req, res, next) => {
-    const { authorization } = req.headers;
-  
-    if (!authorization || !authorization.startsWith('Bearer ')) {
-      return res.status(401).json({
-        success: false,
-        message: "Please login to access this resource"
-      });
-    }
-  
-    const token = authorization.split('Bearer ')[1];
-  
-    try {
-      const decodedToken = await admin.auth().verifyIdToken(token);
-      req.user = decodedToken;
-      next();
-    } catch (error) {
-      return res.status(401).json({
-        success: false,
-        message: "Invalid token"
-      });
-    }
-  };
-=======
 // Check if user is authenticated
 exports.isAuthenticatedUser = async (req, res, next) => {
     const { token } = req.cookies;
@@ -63,7 +23,6 @@ exports.isAuthenticatedUser = async (req, res, next) => {
         });
     }
 };
->>>>>>> Stashed changes
 
 // Handling users roles
 exports.authorizeRoles = (...roles) => {
